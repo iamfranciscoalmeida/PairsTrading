a02d9c8310cec8e7d9e0c7405b437dd6902f5bc5
Francisco Almeida <franciscoalmeida.2001@gmail.com>
2023-07-12 21:46:24 -0400

    Update of stock basket/usr/local/bin/python3

--- pairs.py
+++ pairs.py
@@ -9,21 +9,21 @@ import time
 
 if __name__ == "__main__":
     start_time = time.time()
-    # stocks = read_sp500_tickers("newtickers.csv")
-    # cointegrated_stocks = find_cointegrated_stocks(stocks)
-    # print(cointegrated_stocks)
-    cointegrated_stocks = {'GOOG': ['AAPL'], 'DOV': ['BA'], 'VICI': ['VTR'], 'VZ': ['PARA'], 'TMUS': ['T'], 'BBWI': ['BBY', 'HD'], 'CNC': ['DHR'], 'CVS': ['DHR'], 'EOG': ['COP'], 'XEL': ['SRE'], 'SRE': ['WEC', 'NEE', 'ES', 'AEP', 'DTE', 'DUK', 'LNT'], 'AEP': ['DUK']}
+    stocks = read_sp500_tickers("newtickers.csv")
+    cointegrated_stocks = find_cointegrated_stocks(stocks)
+    print(cointegrated_stocks)
+    # cointegrated_stocks = {'GOOG': ['AAPL'], 'DOV': ['BA'], 'VICI': ['VTR'], 'VZ': ['PARA'], 'TMUS': ['T'], 'BBWI': ['BBY', 'HD'], 'CNC': ['DHR'], 'CVS': ['DHR'], 'EOG': ['COP'], 'XEL': ['SRE'], 'SRE': ['WEC', 'NEE', 'ES', 'AEP', 'DTE', 'DUK', 'LNT'], 'AEP': ['DUK']}
 
 
-    results = {}
-    for key, value in cointegrated_stocks.items():
-        stock1 = key
-        for stock2 in value:
-            results1, results2 = backtest_pairs(stock1, stock2, period="1y")
-            results_key = f"{stock1}/{stock2}"
-            results[results_key] = [results1, results2]
+    # results = {}
+    # for key, value in cointegrated_stocks.items():
+    #     stock1 = key
+    #     for stock2 in value:
+    #         results1, results2 = backtest_pairs(stock1, stock2, period="1y")
+    #         results_key = f"{stock1}/{stock2}"
+    #         results[results_key] = [results1, results2]
             
-    write_results_to_file(results)
+    # write_results_to_file(results)
     end_time = time.time()
     time_taken = round(end_time - start_time, 2)
     print(f"--------Time taken {time_taken}s--------")
